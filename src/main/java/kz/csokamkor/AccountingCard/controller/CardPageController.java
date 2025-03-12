db61c90215a90a11b260d4a307e8a68eb102a75c
akameshev <cryptomonster161193@gtmail.com>
2025-03-12 16:03:07 +0500

    Modified card-detail

--- src/main/java/kz/csokamkor/AccountingCard/controller/CardPageController.java
+++ src/main/java/kz/csokamkor/AccountingCard/controller/CardPageController.java
@@ -3,19 +3,23 @@ package kz.csokamkor.AccountingCard.controller;
 import kz.csokamkor.AccountingCard.model.entities.Card;
 import kz.csokamkor.AccountingCard.model.entities.Inventory;
 import kz.csokamkor.AccountingCard.service.CardService;
+import kz.csokamkor.AccountingCard.service.InventoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/cards")
 public class CardPageController {
     private final CardService cardService;
+    private final InventoryService inventoryService;
 
-    public CardPageController(CardService cardService) {
+    public CardPageController(CardService cardService, InventoryService inventoryService) {
         this.cardService = cardService;
+        this.inventoryService = inventoryService;
     }
 
     @GetMapping
@@ -28,6 +32,8 @@ public class CardPageController {
     public String getCardById(@PathVariable Long id, Model model) {
         model.addAttribute("basket",cardService.findById(id).getCardInventories());
         model.addAttribute("card", cardService.findById(id));
+        List<Inventory> inventories = inventoryService.findAll().get();
+        model.addAttribute("inventories", inventories);
         return "cards/card-detail";
     }
 
@@ -55,6 +61,15 @@ public class CardPageController {
         cardToUpdate.setCardInventories(card.getCardInventories());
         cardService.save(cardToUpdate);
         return "redirect:/cards";
+    }
+
+    @GetMapping("/{id}/add-inventory")
+    public String addInventory(@PathVariable Long id, Model model) {
+        Card card = cardService.findById(id);
+        List<Inventory> inventories = inventoryService.findAll().get();
+        model.addAttribute("card", card);
+        model.addAttribute("inventories",inventories);
+        return "cards/card-inventory-add";
     }
 
 
