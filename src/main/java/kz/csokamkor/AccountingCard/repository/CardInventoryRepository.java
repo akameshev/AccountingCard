b237b6703e41e4fbed666801efc7d81969d016a1
Anuar Kameshev <cryptomonster161193@gmail.com>
2025-03-14 13:57:07 +0500

    Added optional method in CardInventoryRepository

--- src/main/java/kz/csokamkor/AccountingCard/repository/CardInventoryRepository.java
+++ src/main/java/kz/csokamkor/AccountingCard/repository/CardInventoryRepository.java
@@ -4,6 +4,9 @@ import kz.csokamkor.AccountingCard.model.entities.CardInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CardInventoryRepository extends JpaRepository<CardInventory, Long> {
+    Optional<CardInventory> findByName(String name);
 }
